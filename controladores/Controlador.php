2153cb4ee08463853dc92c64b1a201815a50ec29
Edwin Camino <edwinpcamino@gmail.com>
2025-11-12 08:34:12 -0500

    Creamos el controlador principal

--- controladores/Controlador.php
+++ controladores/Controlador.php
@@ -1,10 +1,28 @@
-<?php 
-
-$accion = "";
-$nombre = "";
-
+<?php
 class Controlador {
-    public function __construct() {
-        // Constructor base del controlador
+    public $accion;
+    public $nombreControlador;
+
+    public function llamarAccion($parametros = Array()) {
+        $metodo = $this->accion;
+        if (method_exists($this, $metodo)) {
+            if (empty($parametros)) {
+                $this->$metodo();
+            } else {
+                extract($parametros);
+                $this->$metodo($id);
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function mostrarVista($vista, $parametros = Array()) {
+        extract($parametros, EXTR_PREFIX_SAME, "wddx");
+        ob_start();
+        include "vistas/" . $this->nombreControlador . "/" . $vista . ".php";
+        die();
     }
 }
+?>
