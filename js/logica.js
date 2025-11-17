c72a8a3adc30867514610f7d83a0e17353725e05
allara1-cmyk <allara1@espe.edu.ec>
2025-11-17 01:52:12 -0500

    feat: Agregar funcionabilidad para la eliminacion

--- js/logica.js
+++ js/logica.js
@@ -1,28 +1,34 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
     $(".remover-producto").click(function() {
-    var id = $(this).data("producto");
-    var result = confirm("Desea eliminar el producto?");
-    if (result) {
-        eliminar(id);
-    }
-});
-
-function eliminar(id) {
-    $.ajax({
-        method: "GET",
-        url: "/mvc/index.php/producto/eliminar?id=" + id
-    })
-    .done(function(datos) {
-        datos = JSON.parse(datos);
-        if (datos.ok) {
-            alert(datos.ok);
-            $("#producto_" + id).remove();
-        } else {
-            alert("Error: " + datos.error);
+        var id = $(this).data("producto");
+        var result = confirm("¿Desea eliminar el producto?");
+        if (result) {
+            eliminar(id);
         }
-    })
-    .fail(function() {
-        alert("Error inesperado!");
     });
-}
 
+    function eliminar(id) {
+        $.ajax({
+            method: "GET",
+            url: "/mvc_php/index.php/producto/eliminar?id=" + id
+        })
+        .done(function(datos) {
+            datos = JSON.parse(datos);
+
+            if (datos.ok) {
+                alert(datos.ok);
+                $("#producto_" + id).fadeOut(300, function() {
+                    $(this).remove();
+                });
+
+            } else {
+                alert("Error: " + datos.error);
+            }
+        })
+        .fail(function() {
+            alert("Error inesperado!");
+        });
+    }
+
+});
