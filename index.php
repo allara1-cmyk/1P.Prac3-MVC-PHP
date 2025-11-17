13ead277adfbcf31c7cf488fd5f27453cc52f9f7
allara1-cmyk <allara1@espe.edu.ec>
2025-11-16 23:46:28 -0500

    docs: Agregar comentarios descriptivos

--- index.php
+++ index.php
@@ -1,40 +1,41 @@
 <?php
+// Incluye una vez Controlador y Modelo
 include_once 'controladores/Controlador.php';
 include_once 'modelos/Modelo.php';
 
-if ($_SERVER["REQUEST_URI"] != null) {
+if ($_SERVER["REQUEST_URI"] != null) {                                              // Verifica si existe la URI
     // ruta del servidor
-    $URL_SERVER = explode("index.php/", $_SERVER["REQUEST_URI"])[0];
+    $URL_SERVER = explode("index.php/", $_SERVER["REQUEST_URI"])[0];                // dividir la URl, elimina todo despues de index.php para obtener urta basae
 
     // obtener el controlador desde la URL
-    $recursos = explode(".php/", $_SERVER["REQUEST_URI"]);
-    $recursos = explode("/", isset($recursos[1]) ? $recursos[1] : $recursos[0]);
+    $recursos = explode(".php/", $_SERVER["REQUEST_URI"]);                          // Obtener controlador de la URL
+    $recursos = explode("/", isset($recursos[1]) ? $recursos[1] : $recursos[0]);    // Obtener acción de la URL
 
-    $accion = "";
-    $recursos[1] = (isset($recursos[1])) ? $recursos[1] : "home";
-    if (!empty($_GET)) {
-        $recursos[1] = explode("?", $recursos[1])[0];
+    $accion = "";                                                   
+    $recursos[1] = (isset($recursos[1])) ? $recursos[1] : "home";                   // Si no hay accion definida usar home
+    if (!empty($_GET)) {                                                            // Si hay variables get quitarl cualquier ?param del recurso
+        $recursos[1] = explode("?", $recursos[1])[0];                               
     }
-    $accion = $recursos[1];
+    $accion = $recursos[1];                                                         
 
     // nombre del controlador
-    $nombre = (isset($recursos[0])) ? $recursos[0] : "inicio";
-    $nombre = ucfirst($nombre);
-    require_once "controladores/{$nombre}Controlador.php";
-    $tipo = $nombre . "Controlador";
+    $nombre = (isset($recursos[0])) ? $recursos[0] : "inicio";                      // Obtener el nombre del controlador
+    $nombre = ucfirst($nombre);                                                     // Primer letra mayuscula
+    require_once "controladores/{$nombre}Controlador.php";                          // Carga el archivo del controlador
+    $tipo = $nombre . "Controlador";                                                // Dar nombre al controlador    
 
     // crear controlador
-    $controlador = new $tipo;
-    $controlador -> accion = $accion;
-    $controlador -> nombreControlador = $nombre;
+    $controlador = new $tipo;                                                       // Instancia el controlador
+    $controlador -> accion = $accion;                                               // Asignar acción y nombre del controlador
+    $controlador -> nombreControlador = $nombre;                                    
     
     // enviar parametros al controlador
-    $parametros = (!empty($_GET)) ? $_GET : Array();
-    $llamado = $controlador -> llamarAccion($parametros);
-    if (!$llamado) {
+    $parametros = (!empty($_GET)) ? $_GET : Array();                                // Si hay parámetros GET, se los paso al controlador           
+    $llamado = $controlador -> llamarAccion($parametros);                           // Llamar a la acción, método del controlador
+    if (!$llamado) {                                                                // Si no se encuentra la ruta muestra error
         echo "Ruta no encontrada.";
-    }
+    }   
 } 
-else {
-    echo "No hay ruta valida.";
+else {  
+    echo "No hay ruta valida.";                                                     // Si no hay REQUEST_URI, mostrar error
 }
