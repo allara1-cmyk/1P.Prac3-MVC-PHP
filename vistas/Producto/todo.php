153a205372014eae1833071bba100f81502d69c0
allara1-cmyk <allara1@espe.edu.ec>
2025-11-17 01:49:52 -0500

    refactor: Corregir rutas en botones de acciones

--- vistas/Producto/todo.php
+++ vistas/Producto/todo.php
@@ -1,7 +1,6 @@
 <div class="container">
     <div class="row">
         <div class="col-12 col-lg-8 offset-lg-2">
-
             <table class="table table-striped">
                 <thead>
                     <tr>
@@ -12,36 +11,27 @@
                         <th></th>
                     </tr>
                 </thead>
-
                 <tbody>
-                <?php
-                for($i = 0; $i < count($resultados); $i++){
-                    echo "<tr id='producto_" . $resultados[$i]->idProducto . "'>";
-                    
-                    echo "<td>" . $resultados[$i]->nombre . "</td>";
-                    echo "<td>" . $resultados[$i]->precio . "</td>";
-                    echo "<td>" . $resultados[$i]->cantidad . "</td>";
+                    <?php
+                    //Cuerpo de la tabla generado dinámicamente con PHP
+                    for ($i = 0; $i < count($resultados); $i++) {
+                        echo '<tr id="producto_' . $resultados[$i]->idProducto . '">';
+                        echo '<td>' . $resultados[$i]->nombre . '</td>';
+                        echo '<td>' . $resultados[$i]->precio . '</td>';
+                        echo '<td>' . $resultados[$i]->cantidad . '</td>';
+                        echo '<td><a href="/mvc_php/index.php/producto/actualizar?id=' . $resultados[$i]->idProducto . '"><i class="fa fa-edit"></i></a></td>';
+                        echo '<td><span class="remover-producto" data-producto="' . $resultados[$i]->idProducto . '">
+                                <i class="fa fa-remove" style="cursor:pointer;color:red"></i>
+                            </span></td>';
 
-                    echo "<td><a href=\"/mvc/index.php/producto/actualizar?id=" 
-                         . $resultados[$i]->idProducto . "\">
-                         <i class='fa fa-edit'></i></a></td>";
 
-                    echo "<td><span data-producto=\"" 
-                         . $resultados[$i]->idProducto . "\">
-                         <i class='fa fa-remove'></i></span></td>";
 
-                    echo "</tr>";
-                }
-                ?>
+                        echo '</tr>';
+                    }
+                    ?>
                 </tbody>
-
             </table>
-
-            <a class="btn btn-primary" 
-               href="/mvc/index.php/producto/crear">
-               Crear producto
-            </a>
-
+            <a class="btn btn-primary" href="/mvc_php/index.php/producto/crear">Crear producto</a>
         </div>
     </div>
 </div>
