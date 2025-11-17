38493367ce02ffc56f350d21fe023aafb3fd5617
allara1-cmyk <allara1@espe.edu.ec>
2025-11-17 01:48:50 -0500

    feat: Crear funcionablidad para crear

--- vistas/Producto/crear.php
+++ vistas/Producto/crear.php
@@ -1,20 +1,12 @@
 <h3>Crear producto</h3>
-
 <?php
-    if (isset($error) && $error){
-        echo "Valida los datos";
-    }
+if (isset($error) && $error){
+    echo "valida los datos";
+}
 ?>
-
 <form method="post">
-    <input type="text" placeholder="nombre" 
-           name="Producto[nombre]">
-
-    <input type="number" placeholder="precio" 
-           name="Producto[precio]">
-
-    <input type="number" placeholder="cantidad" 
-           name="Producto[cantidad]">
-
+    <input type="text" placeholder="nombre" name="producto[nombre]">
+    <input type="number" placeholder="precio" name="producto[precio]">
+    <input type="number" placeholder="cantidad" name="producto[cantidad]">
     <button type="submit" name="Guardar">Crear Producto</button>
 </form>
