a46a4559a437d3314857d80e97fc9061f00fdd78
allara1-cmyk <allara1@espe.edu.ec>
2025-11-16 22:07:50 -0500

    feat: modificar clase InicioControlador y agregar comentarios descriptivos

--- controladores/InicioControlador.php
+++ controladores/InicioControlador.php
@@ -1,6 +1,9 @@
 <?php
+// Controlador especifico que hereda de class Controlador
 class InicioControlador extends Controlador {
-    public function home() {
+    // Metodo propio 
+    function home(){
+        // Carga la vista home dentro del directorio inicio
         $this->mostrarVista("home", $_GET);
     }
 }
